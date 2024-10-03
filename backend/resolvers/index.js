242b243481d1df447842e8a0805fb1dd50c66e0c
duyentth <duyen.tran@miu.edu>
2024-10-02 21:31:53 -0400

    first commit

---
+++ backend/resolvers/index.js
@@ -0,0 +1,6 @@
+import userResolver from "./user.resolver.js";
+import transactionResolver from "./transaction.resolver.js";
+import { mergeResolvers } from "@graphql-tools/merge";
+
+const mergedResolvers = mergeResolvers([userResolver, transactionResolver]);
+export default mergedResolvers;
